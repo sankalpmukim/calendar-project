d724b7d32a7c9ef8b383463e642e67f1f3be0055
Sankalp Mukim <sankalpmukim@gmail.com>
2023-04-14 00:43:54 +0530

    setup fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,10 @@
 import { type NextPage } from "next";
 import Head from "next/head";
-import Link from "next/link";
 import Layout from "~/components/Layout";
 
 import { api } from "~/utils/api";
 
 const Home: NextPage = () => {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
-
   return (
     <>
       <Head>
